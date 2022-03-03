e9979e0fd4ed03b3c405bcdafab3b5fe187118fe
AirWav2021 <AirWav2015@gmail.com>
2022-03-03 23:00:45 +0300

    Выполнил ДЗ 7

---
+++ Lesson-7/src/store/index.js
@@ -0,0 +1,28 @@
+import { createStore, combineReducers, compose, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
+import { persistReducer, persistStore } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+import { profileReducer } from './profile/reducer'
+import { chatsReducer } from './chats/reducer'
+import { messagesReducer } from './messages/reducer'
+
+const rootReducer = combineReducers({
+	profile: profileReducer,
+	chats: chatsReducer,
+	messages: messagesReducer,
+})
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
+const persistConfig = {
+	key: 'myMessenger',
+	storage,
+	blacklist: [''],
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+export const store = createStore(
+	persistedReducer,
+	composeEnhancers(applyMiddleware(thunk)),
+)
+export const persistor = persistStore(store)
